1a398f09c02090ed29d3630267e7ea5c3d3b781d
akkabb <akkabb@gmx.fr>
2023-04-02 08:05:22 +0200

    init of Day2

---
+++ Day2/index.php
@@ -0,0 +1,40 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Manipuler les structures de base : Les conditions</title>
+</head>
+<body>
+    <div class="container">
+        <header>
+            <img src="" alt="">
+            <nav>
+                <ul>
+                    <li><a href="exo1.php">Exercice 1</a></li>
+                    <li><a href="exo2.php">Exercice 2</a></li>
+                    <li><a href="exo3.php">Exercice 3</a></li>
+                </ul>
+            </nav>
+        </header>
+        <main>
+            <section class="main_content">
+                <p class="main_title">
+                    La programmation est une compétence en constante évolution et il est important de continuer à la développer. C'est pourquoi j'ai décidé de m'engager dans une série d'éxercices de code quotidien.
+                    En travaillant sur des problèmes de programmation chaque jour, je m'exerce à résoudre des problèmes complexes de manière efficace et créative.
+                    Je suis convaincu que cette pratique quotidienne me permettra de progresser rapidement et de manière significative dans ma carrière de développeur et dans la résolution de problèmes informatiques en général.
+                    Enfin, je pense que cette démarche m'aidera à développer une plus grande confiance en moi-même en tant que développeur et à être plus à l'aise avec les défis de programmation que je pourrais rencontrer à l'avenir.
+                </p>
+            </section>
+        </main>
+        <footer class="footer">
+            <p>© 2023 akkabb, Inc.</p>
+        </footer>
+    </div>  
+</body>
+</html>
