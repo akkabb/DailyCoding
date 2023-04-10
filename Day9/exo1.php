21a6d672d4125d520deb8b222961d59e84f3524e
akkabb <akkabb@gmx.fr>
2023-04-10 17:15:15 +0200

    Day 9 not done, need to be re-start

---
+++ Day9/exo1.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <? require_once('include/head.php');?>
+    <title>Exo 1</title>
+</head>
+<body>
+    <div class="container">
+        <? require_once('include/header.php') ?>
+        <h2>Enonce</h2>
+        <div class="wrapper">
+            <h3>1.Ecrire un script qui calcule la somme de deux matrices carrées.</h3>
+        </div>
+        <div class="answer">
+            <h3>Ma solution</h3>
+            <p>Je fais de mon mieux pour proposer une solution :</p>
+            <p><em>Je n'ai pas réussi l'exercice</em> ... <br> A refaire</p>
+            <h3>LINKS : </h3>
+            <p>lien vers la <a href="https://www.exelib.net/php/calculs-sur-des-tableaux-multidimensionnels-matrices.html#solution-tab">corection</a> : </p>
+            <?php
+                
+            ?>
+        </div>
+        <hr>
+        <div class="wrapper">
+            <h3>2.Ercire un script qui calcule le produit de deux matrices, sachant que le produit de deux matrices carrées est :</h3>
+            <img src="img/DailyCoding_April_day9.png" alt="image de l'énoncé" width="50%">
+        </div>
+        <div class="answer">
+        <p>Je fais de mon mieux pour proposer une solution :</p>
+            <p><em>Je n'ai pas réussi l'exercice</em> ... <br> A refaire</p>
+            <h3>LINKS : </h3>
+            <p>lien vers la <a href="https://www.exelib.net/php/calculs-sur-des-tableaux-multidimensionnels-matrices.html#solution-tab">corection</a> : </p>
+        </div>
+        <? require_once('include/footer.php') ?>
+    </div>
+</body>
+</html>
---
+++ Day9/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <? require_once('include/head.php');?>
+    <title>Calculs sur des tableaux multidimensionnels ( Matrices) </title>
+</head>
+<body>
+    <div class="container">
+        <? require_once('include/header.php'); ?>
+        <main>
+            <h1>Daily Coding</h1>
+            <section class="main_content">
+                <p class="main_title">
+                    La programmation est une compétence en constante évolution et il est important de continuer à la développer. C'est pourquoi j'ai décidé de m'engager dans une série d'éxercices de code quotidien.
+                    En travaillant sur des problèmes de programmation chaque jour, je m'exerce à résoudre des problèmes complexes de manière efficace et créative.
+                    Je suis convaincu que cette pratique quotidienne me permettra de progresser rapidement et de manière significative dans ma carrière de développeur et dans la résolution de problèmes informatiques en général.
+                    Enfin, je pense que cette démarche m'aidera à développer une plus grande confiance en moi-même en tant que développeur et à être plus à l'aise avec les défis de programmation que je pourrais rencontrer à l'avenir.
+                </p>
+            </section>
+            <section class="objectif">
+                <h2 class="objectif_title">Objéctif :</h2>
+                <h3 class="objectif_goal">Manipuler les tableaux multidimensionnels.</h3>
+                <h3 class="objectif_goal">Effectuer des claculs matricielle.</h3>
+            </section>
+
+        </main>
+        <? require_once('include/footer.php');?>
+    </div>  
+</body>
+</html>
