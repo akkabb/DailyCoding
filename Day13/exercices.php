a344a0178f1b923d96c87af31a3658e649ceceb2
akkabb <akkabb@gmx.fr>
2023-04-14 05:55:24 +0200

    Exercices have been done | need css

--- Day13/exercices.php
+++ Day13/exercices.php
@@ -12,143 +12,157 @@
     <h2>Exercices pour débutant</h2>
     <section class="exo6 ">
         <div class="wrapper">
-            <h3>Fonction qui retourne Hello world</h3>
+            <h3>Créer une fonction from scratch qui s'appelle <strong>somme()</strong>. <br> Elle prendra deux arguments de type int. Elle devra retourner la somme des deux. Exemple : argument 1 = 5 Argument 2 = 5 ; Resultat : 10</h3>
         </div>
         <div class="answer">
             <h3>Voici mon essai : </h3>
             <?
-            function greetings()
+            function somme($a, $b)
             {
-                echo "Hello world";
+                return $a + $b;
             }
-            greetings();
+            echo somme(5,10);
             ?>
             <h3>Voici mon code :</h3>
             <p class="monCode">
-            function greetings()
-            {<br>
-                return "Hello world";<br>
+            function somme($a, $b)
+            { <br>
+                return $a + $b; <br>
             }
+            echo somme(5,10);
             </p>
         </div>
         <div class="comment">
             <h3>Mon point de vue :</h3>
-            <p>Cet exercice est interessant pour afficher son premier texte en php<br> ça change du simple echo</p>
+            <p>Deux arguments et cette fois, de int au lieu de string</p>
         </div>
     </section>
     <section class="exo7 ">
         <div class="wrapper">
-            <h3>Créer une fonction from scratch qui s'appelle quiEstLeMeilleurProf(). <br> Elle doit retourner <span>Le prof de programmation web</span> </h3>
+            <h3>Créer une fonction from scratch qui s'appelle <strong>soustraction()</strong>. <br> Elle prendra deux arguments de type int. Elle devra retourner la soustraction des deux. Exemple : argument 1 = 5 Argument 2 = 5 ; Resultat : 0</h3>
         </div>
         <div class="answer">
             <h3>Voici mon essai : </h3>
             <?
-            function quiEstLeMeilleurProf()
+            function soustraction($a, $b)
             {
-                echo "Le prof de programmation web";
+                return $a - $b;
             }
 
-            echo quiEstLeMeilleurProf();
+            echo soustraction(5, 5);
             ?>
             <h3>Voici mon code :</h3>
             <p class="monCode">
-            function quiEstLeMeilleurProf()
+            function soustraction()
             {<br>
-                echo "Le prof de programmation web";<br>
+                return $a - $b; <br>
             }
+            <br>
+            echo soustraction(5, 5);
             </p>
         </div>
         <div class="comment">
             <h3>Mon point de vue :</h3>
-            <p>Lorsque la réussite est là , ça fait plaisir</p>
+            <p>Variante de l'exercice précédent, et avec le signe (-) cette fois.</p>
         </div>
     </section>
     <section class="exo8">
         <div class="wrapper">
-            <h3>Créer une fonction from scratch qui s'appelle <strong>jeRetourneMonArgument()</strong>. Exemple : Arg = "abc" ==> Return abc Arg = 123 ==> Return 123</h3>
+            <h3>Créer une fonction from scratch qui s'appelle <strong>multiplication()</strong>. <br> Elle prendra deux arguments de type int. Elle devra retourner la multiplication des deux. Exemple : argument 1 = 5 Argument 2 = 5 ; Resultat : 25</h3>
         </div>
         <div class="answer">
             <h3>Voici mon essai : </h3>
             <?
-            function jeRetourneMonArgument($arg)
+            function multiplication($num, $num1)
             {
-                
-                return $arg;
+                return $num * $num1;
             }
 
-            echo jeRetourneMonArgument(123);
+            echo multiplication(1, 5);
             ?>
             <h3>Voici mon code :</h3>
             <p class="monCode">
-            function jeRetourneMonArgument($arg)
+            function multiplication($num, $num1)
             {<br>
-                return $arg;<br>
+                return $num * $num1;<br>
             }
 
-            echo jeRetourneMonArgument(123);
+            echo multiplication(1, 5);
             </p>
         </div>
         <div class="comment">
             <h3>Mon point de vue :</h3>
-            <p>Un argument ; un truc dans la parethèse de l afonctio quoi!</p>
+            <p>Variante de l'exercice précédent, et avec le signe (*) cette fois.</p>
         </div>
     </section>
     <section class="exo9">
         <div class="wrapper">
-            <h3>Créer une fonction from scratch qui s'appelle <strong>concatenation()</strong>. Elle prendra deux arguments de type string. Elle devra retourner la concatenation des deux. Exemple : argument 1 = Antoine Argument 2 = Griezmann; Resultat : AntoineGriezmann</h3>
+            <h3>Créer une fonction from scratch qui s'appelle <strong>estIlMajeure()</strong>. Elle prendra un argument de type int. Elle devra retourner un boolean. <br> Si age >= 18 elle doit retourner true si age < 18 elle doit retourner false Exemple : age = 5 ==> false age = 34 ==> true</h3>
         </div>
         <div class="answer">
             <h3>Voici mon essai : </h3>
             <?
-            function concatenation($str, $str1)
+            function estIlMajeure($age)
             {
-                return $str . $str1;
+                if($age >= 18)
+                    echo "true";
+                else
+                    echo "false";
             }
 
-            echo concatenation("Antoine", "Griezmann");
+            estIlMajeure(5);
             ?>
             <h3>Voici mon code :</h3>
             <p class="monCode">
-            function concatenation($str, $str1)
+            function estIlMajeure($age)
             {<br>
-                return $str . $str1;<br>
+                if($age >= 18) <br>
+                    echo "true"; <br>
+                else  <br>
+                    echo "false"; <br>
             }
 
-            echo concatenation("Antoine", "Griezmann");
+            estIlMajeure(5);
             </p>
         </div>
         <div class="comment">
             <h3>Mon point de vue :</h3>
-            <p>La concaténation : ajoute un élément au premier!??</p>
+            <p>Je dois encore répéter le return et le echo dans les fonctions; ce n'est pas encore une seconde nature</p>
         </div>
     </section>
     <section class="exo10">
         <div class="wrapper">
-            <h3>Créer une fonction from scratch qui s'appelle <strong>concatenationAvecEspace()</strong>. Elle prendra deux arguments de type string. Elle devra retourner la concatenation des deux. <br> Exemple : argument 1 = Ngolo Argument 2 = Kante; Resultat : Ngolo Kante</h3>
+            <h3>Créer une fonction from scratch qui s'appelle plusGrand(). <br> Elle prendra deux arguments de type int. Elle devra retourner le plus grand des deux.</h3>
         </div>
         <div class="answer">
             <h3>Voici mon essai : </h3>
             <?
-            function concatenationAvecEspace($str, $str1)
+            function plusGrand($int, $num)
             {
-                return $str . " " .$str1;
+                if ($int > $num)
+                    return $int;
+                else
+                    return $num;
             }
 
-            echo concatenationAvecEspace("Ngolo", "Kante");
+            echo plusGrand(9, 5);
             ?>
             <h3>Voici mon code :</h3>
             <p class="monCode">
-            function concatenationAvecEspace($str, $str1)
+            function plusGrand($int, $num)
             {<br>
-                return $str . " " .$str1;<br>
+                if ($int > $num) <br>
+                    return $int; <br>
+                else  <br>
+                    return $num; <br>
             }
 
-            echo concatenationAvecEspace("Ngolo", "Kante");
+            echo plusGrand(9, 5);
             </p>
         </div>
         <div class="comment">
             <h3>Mon point de vue :</h3>
-            <p></p>
+            <p> Continuer à s'exercer</p>
         </div>
     </section>
 </body>
