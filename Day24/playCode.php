408fcf578ade697d8207dc291bf8bf2117faf32b
akkabb <akkabb@gmx.fr>
2023-04-24 07:24:54 +0200

    Basic structure done | remains to do the exercises

---
+++ Day24/playCode.php
@@ -0,0 +1,383 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Daily Coding</title>
+</head>
+<body>
+    <div class="container">
+        <h1>PHP basic</h1>
+        <main>
+            <section class="exo13">
+                <h2>Exercice 13</h2>
+                <div class="resumeWrapper">
+                    <h3>Write a e PHP script to display string, values within a table. </h3>
+                </div>
+                <div class="resumeOutputExple">
+                    <p><strong>Note : </strong> Use HTML table elements into echo</p>
+                    <p><strong>Expected Output :</strong></p>
+                    <table>
+                            <tr>
+                                <td>Salary of Mr.A is</td>
+                                <td>1000$</td>
+                            </tr>
+                            <tr>
+                                <td>Salary of Mr.B is</td>
+                                <td>1200$</td>
+                            </tr>
+                            <tr>
+                                <td>Salary of Mr.C is</td>
+                                <td>14S00$</td>
+                            </tr>
+                        
+                    </table>
+                </div>
+                <div class="answerWrapper">
+                    <div class="mySol">
+                        <h3>I try my best : </h3>
+                        <?php 
+                        
+                        ?>
+                    </div>
+                    <div class="myCode">
+                        <h3>I show you my script : </h3>
+                        <p>
+                            &lt?php <br>
+                               
+                            ?&gt
+                        </p>
+                    </div>
+                    <div class="comment_area">
+                        <h3>My point of view :</h3>
+                        <p class="comment_style">
+                           
+                        </p>
+                        <p>There's a link to the solution given by the website : <a href="">solution</a></p>
+                    </div>
+                </div>
+            </section>
+            <section class="exo14">
+            <h2>Exercice 14</h2>
+                <div class="resumeWrapper">
+                    <h3>Write a PHP script to display source code of a webpage (e.g. "http://www.example.com/").</h3>
+                    <div class="answerWrapper">
+                        <div class="mySol">
+                            <h3>Lets GO!!</h3>
+                            <?php
+                             
+                            ?>
+                        </div>
+                        <div class="myCode">
+                            <h3>My script :</h3>
+                            <p>
+                                &lt?php <br>
+                                    echo "Tomorrow I 'll learn PHP global variables." . '&ltbr&gt' . "This is a bad command : del c:\*.* "; <br>    
+                                ?&gt
+                            </p>
+                        </div>
+                        <div class="comment_area">
+                            <h3>My point of view :</h3>
+                            <p> </p>
+                            <p>There's a link to the solution given by the website : <a href="">solution</a></p>
+                        </div>
+                    </div>
+                </section>
+                <section class="exo15">
+                    <h2>Exercice 15</h2>
+                    <div class="resumeWrapper">
+                        <h3>Write a PHP script to get last modified information of a file.</h3>
+                    </div>
+                    <div class="resumeOutputExple">
+                        <p><strong>Sample filename :</strong> php-basic-exercises.php</p>
+                        <p><strong>Sample Output :</strong> Last modified Monday, 26th June, 2017, 12:43pm</p>
+                    </div>
+                    <div class="answerWrapper">
+                        <div class="mySol">
+                            <H3>Try to solve it</H3>
+                            <?php
+                                
+                            ?>
+                            <div id="here">
+
+                            </div>
+                        </div>
+                        <div class="myCode">
+                            <h3>Sample script : </h3>
+                            <p>
+                                &lt?php <br>
+                                
+                                ?&gt
+                            </p>
+                        </div>
+                        <div class="comment_area">
+                            <h3>My point of view :</h3>
+                            <p>
+                                
+                            </p>
+                            <p>There's a link to the solution given by the website : <a href="">solution</a></p>
+                        </div>
+                    </div>
+                </section>
+            <section class="exo16">
+            <h2>Exercice 16</h2>
+                <div class="resumeWrapper">
+                    <h3>Write a PHP script to count number of lines in a file.</h3>
+                    <div class="resumeOutputExple">
+                        <p><strong>Note : </strong> Store a text file name into a variable and count the number of lines of text it has.</p>
+                    </div>
+                </div>
+                <div class="answerWrapper">
+                    <div class="mySol">
+                        <h3>I will try my best :</h3>
+                       
+                        <?php
+                            
+                        ?>
+                    </div>
+                    <div class="myCode">
+                        <h3>Show my script :</h3>
+                        <p>
+                            &lt?php <br>
+                                
+                            ?&gt
+                        </p>
+                    </div>
+                    <div class="comment_area">
+                        <h3>my point of view</h3>
+                        <p></p>
+                        <p>There's a link to the solution given by the website : <a href="">solution</a></p>
+                    </div>
+                </div>
+            </section>
+            <section class="exo17">
+            <h2>Exercice 17</h2>
+                <div class="resumeWrapper">
+                    <h3>Write a PHP script to print current PHP version.</h3>
+                    <div class="resumeOutputExple">
+                        <p><strong>Note : </strong> Do not use phpinfo() function.</p>
+                    </div>
+                </div>
+                <div class="answerWrapper">
+                    <div class="mySol">
+                        <h3>Go GO GO !!!</h3>
+                        <?php
+                            
+                        ?>
+                    </div>
+                    <div class="myCode">
+                        <h3>Show my script :</h3>
+                        <p>
+                            &lt?php <br>
+                                
+                            ?&gt
+                        </p>
+                    </div>
+                    <div class="comment_area">
+                        <h3>my point of view</h3>
+                        <p>There's a link to the solution given by the website : <a href="">solution</a></p>
+                    </div>
+                </div>
+            </section>
+            <section class="exo18">
+            <h2>Exercice 18</h2>
+                <div class="resumeWrapper">
+                    <h3>Write a PHP script to delay the program execution for the given number of seconds.</h3>
+                </div>
+                <div class="answerWrapper">
+                    <div class="mySol">
+                        <h3>Try to solve it :</h3>
+                        <?php
+                            
+                        ?>
+                    </div>
+                    <div class="myCode">
+                        <h3>Show my script :</h3>
+                            <p>
+                                &lt?php <br>
+                                    
+                                ?&gt
+                            </p>
+                    </div>
+                    <div class="comment_area">
+                        <h3>my point of view</h3>
+                        <p>There's a link to the solution given by the website : <a href="">solution</a></p>
+                        <p></p>
+                    </div>
+                </div>
+            </section>
+            <section class="exo19">
+            <h2>Exercice 19</h2>
+                <div class="resumeWrapper">
+                    <h3>Arithmetic operations on character variables : $d = 'A00'. Using this variable print the following numbers.</h3>
+                    <div class="resumeOutputExple">
+                        <p><strong>Sample Output : </strong></p>
+                        <p>A01 <br> A02 <br>  A03 <br> A04 <br>  A05</p>
+                    </div>
+                </div>
+                <div class="answerWrapper">
+                    <div class="mySol">
+                        <h3>Let's GO !!</h3>
+                        <?php
+                        ?>
+                    </div>
+                    <div class="myCode">
+                        <h3>Show my script :</h3>
+                            <p>
+                                &lt?php <br>
+                                    
+                                ?&gt
+                            </p>
+                    </div>
+                    <div class="comment_area">
+                        <p>There's a link to the solution given by the website : <a href="">solution</a></p>
+                        <p>I google it to and this <a href="https://www.php.net/manual/en/function.getcwd.php">link</a> helped me!</p>
+                    </div>
+                </div>
+            </section>
+            <section class="exo20">
+            <h2>Exercice 20</h2>
+                <div class="resumeWrapper">
+                    <h3>Write a PHP script to get the last occurred error.</h3>
+                </div>
+                <div class="answerWrapper">
+                    <div class="mySol">
+                        <h3>Ready , Go :</h3>
+                        <?php
+                            
+                        ?>
+                    </div>
+                    <div class="myCode">
+                        <h3>Showing my script :</h3>
+                        <p>
+                            &lt?php <br>
+                                    
+                            ?&gt
+                        </p>
+                    </div>
+                    <div class="comment_area">
+                        <h3>my point of view</h3>
+                        <p>There's a link to the solution given by the website : <a href="">solution</a></p>
+                        <p>I can't manage without google. Nice to search and find the sol</p>
+                    </div>
+                </div>
+            </section>
+            <section class="exo21">
+            <h2>Exercice 21</h2>
+                <div class="resumeWrapper">
+                    <h3>Write a PHP function to test whether a number is greater than 30, 20 or 10 using ternary operator.</h3>
+                </div>
+                <div class="answerWrapper">
+                    <div class="mySol">
+                    <?php
+                        
+                    ?>
+                    </div>
+                    <div class="myCode">
+                        <h3>My Script : </h3>
+                        <p>
+                            &lt?php <br>
+                                    
+                            ?&gt
+                        </p>
+                    </div>
+                    <div class="comment_area">
+                        <h3>my point of view</h3>
+                        <p></p>
+                        <p>share the link to the <a href="https://www.w3resource.com/php-exercises/php-basic-exercise-9.php">solution</a></p>
+                    </div>
+                </div>
+            </section>
+            <section class="exo22">
+            <h2>Exercice 22</h2>
+                <div class="resumeWrapper">
+                    <h3>Write a PHP script to get the full URL. </h3>
+                </div>
+                <div class="answerWrapper">
+                    <div class="mySol">
+                        <h3>Solved IT !</h3>
+                        <?php
+                            
+                        ?>
+                    </div>
+                    <div class="myCode">
+                        <h3>Show my code</h3>
+                        <p>
+                            &lt?php <br>
+                                    
+                            ?&gt 
+                        </p>
+                    </div>
+                    <div class="comment_area">
+                        <h3>my point of view</h3>
+                        <p>There's a link to the solution given by the website : <a href="">solution</a></p>
+                        <p></p>
+                    </div>
+                </div>
+            </section>
+            <section class="exo23">
+            <h2>Exercice 23</h2>
+                <div class="resumeWrapper">
+                    <h3>Write a PHP script to compare the PHP version . </h3>
+                    <div class="resumeOutputExple">
+                        <p><strong>Note :</strong> Use version_compare() function and PHP_VERSION constant.</p>
+                    </div>
+                </div>
+                <div class="answerWrapper">
+                    <div class="mySol">
+                        <h3>Go Ahead</h3>
+                        <?php
+                            
+                        ?>
+                    </div>
+                    <div class="myCode">
+                        <h3>Showing my code</h3>
+                        <p>
+                            &lt?php <br>
+                                    
+                            ?&gt 
+                        </p>
+                    </div>
+                    <div class="comment_area">
+                        <h3>my point of view</h3>
+                        <p>There's a link to the solution given by the website : <a href="">solution</a></p>
+                        <p>it helps to do that more often, i had this exercice few days before. i felt comfortable</p>
+                    </div>
+                </div>
+            </section>
+            <section class="exo24">
+            <h2>Exercice 24</h2>
+                <div class="resumeWrapper">
+                    <h3>Write a PHP script to get the name of the owner of the current PHP script</h3>
+                </div>
+                <div class="answerWrapper">
+                    <div class="mySol">
+                        <h3>Let's try </h3>
+                        <?php
+                           
+                        ?>
+                    </div>
+                    <div class="myCode">
+                        <h3> I show you my code</h3>
+                        <p>
+                            &lt?php <br>
+                                    
+                            ?&gt 
+                        </p>
+                    </div>
+                    <div class="comment_area">
+                        <h3>my point of view</h3>
+                        <p>There's a link to the solution given by the website : <a href="">solution</a></p>
+                        <p>Not easy for me. Need to google it and see the solution to be more confident. </p>
+                    </div>
+                </div>
+            </section>
+        </main>
+        <footer>
+            <p>© 2023 akkabb, Inc.</p>
+        </footer>
+    </div>
+</body>
+</html>
