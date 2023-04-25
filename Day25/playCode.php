cc18587649b700ebbadb6f1ccfaf7339e2602e65
akkabb <akkabb@gmx.fr>
2023-04-25 06:32:49 +0200

    Yesterday style ; + Today daily basement ; push

---
+++ Day25/playCode.php
@@ -0,0 +1,361 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Daily Coding</title>
+</head>
+<body>
+    <div class="container">
+        <h1>PHP basic</h1>
+        <main>
+            <section class="exo25">
+                <h2>Exercice 25</h2>
+                <div class="resumeWrapper">
+                    <h3>Write a PHP script to get the document root directory under which the current script is executing, as defined in the server's configuration file.</h3>
+                </div>
+                <div class="answerWrapper">
+                    <div class="mySol">
+                        <h3>I try my best : </h3>
+                        <?php 
+                        ?>
+                    </div>
+                    <div class="myCode">
+                        <h3>I show you my script : </h3>
+                        <p>
+                            &lt?php <br>
+                            
+                            ?&gt
+                        </p>
+                    </div>
+                    <div class="comment_area">
+                        <h3>My point of view :</h3>
+                        <p>
+                           
+                        </p>
+                        <p>There's a link to the solution given by the website : <a href="https://www.w3resource.com/php-exercises/php-basic-exercise-13.php">solution</a></p>
+                    </div>
+                </div>
+            </section>
+            <section class="exo26">
+            <h2>Exercice 26</h2>
+                <div class="resumeWrapper">
+                    <h3>Write a PHP script to get the information about the operating system PHP is running on.</h3>
+                    <div class="answerWrapper">
+                        <div class="mySol">
+                            <h3>Lets GO!!</h3>
+                            <?php
+                             
+                            ?>
+                        </div>
+                        <div class="myCode">
+                            <h3>My script :</h3>
+                            <p>
+                                &lt?php <br>
+                                    
+                                ?&gt
+                            </p>
+                        </div>
+                        <div class="comment_area">
+                            <h3>My point of view :</h3>
+                            <p></p>
+                            <p>There's a link to the solution given by the website : <a href="https://www.geeksforgeeks.org/how-to-get-the-source-code-of-a-web-page-using-php/">solution</a></p>
+                        </div>
+                    </div>
+                </section>
+                <section class="exo27">
+                    <h2>Exercice 27</h2>
+                    <div class="resumeWrapper">
+                        <h3>Write a PHP script to print out all the credits for PHP.</h3>
+                    </div>
+                    <div class="answerWrapper">
+                        <div class="mySol">
+                            <H3>Try to solve it</H3>
+                            <?php
+                               
+                            ?>
+                        </div>
+                        <div class="myCode">
+                            <h3>Sample script : </h3>
+                            <p>
+                                &lt?php <br>
+                                
+                                ?&gt <br>
+                            </p>
+                        </div>
+                        <div class="comment_area">
+                            <h3>My point of view :</h3>
+                            <p>
+                                
+                            </p>
+                            <p>There's a link to the solution given by the website : <a href="https://www.w3resource.com/php-exercises/php-basic-exercise-15.php">solution</a></p>
+                        </div>
+                    </div>
+                </section>
+            <section class="exo28">
+            <h2>Exercice 28</h2>
+                <div class="resumeWrapper">
+                    <h3>Write a PHP script to get the directory path used for temporary files.</h3>
+                </div>
+                <div class="answerWrapper">
+                    <div class="mySol">
+                        <h3>I will try my best :</h3>
+                       
+                        <?php
+                            
+                        ?>
+                    </div>
+                    <div class="myCode">
+                        <h3>Show my script :</h3>
+                        <p>
+                            &lt?php <br>
+                                
+                            ?&gt
+                        </p>
+                    </div>
+                    <div class="comment_area">
+                        <h3>my point of view</h3>
+                        <p>I google it and we can compare it with the <a href="https://www.w3resource.com/php-exercises/php-basic-exercise-16.php">solution</a> </p>
+                        <p></p>
+                    </div>
+                </div>
+            </section>
+            <section class="exo29">
+            <h2>Exercice 29</h2>
+                <div class="resumeWrapper">
+                    <h3>Write a PHP script to get the names of the functions of a module</h3>
+                    <div class="resumeOutputExple">
+                        <p><strong>Note : </strong> Find XML, JSON functions etc..</p>
+                    </div>
+                </div>
+                <div class="answerWrapper">
+                    <div class="mySol">
+                        <h3>Go GO GO !!!</h3>
+                        <?php
+                            
+                        ?>
+                    </div>
+                    <div class="myCode">
+                        <h3>Show my script :</h3>
+                        <p>
+                            &lt?php <br>
+                                
+                            ?&gt
+                        </p>
+                    </div>
+                    <div class="comment_area">
+                        <h3>my point of view</h3>
+                        <p>There's a link to the solution given by the website : <a href="https://www.w3resource.com/php-exercises/php-basic-exercise-17.php">solution</a></p>
+                    </div>
+                </div>
+            </section>
+            <section class="exo30">
+            <h2>Exercice 30</h2>
+                <div class="resumeWrapper">
+                    <h3>Write a PHP script to get the time of the last modification of the current page.</h3>
+                </div>
+                <div class="answerWrapper">
+                    <div class="mySol">
+                        <h3>Try to solve it :</h3>
+                        <?php
+                            
+                        ?>
+                    </div>
+                    <div class="myCode">
+                        <h3>Show my script :</h3>
+                            <p>
+                                &lt?php <br>
+                                    
+                                ?&gt
+                            </p>
+                    </div>
+                    <div class="comment_area">
+                        <h3>my point of view</h3>
+                        <p>There's a link to the solution given by the website : <a href="https://www.w3resource.com/php-exercises/php-basic-exercise-18.php">solution</a></p>
+                    
+                    </div>
+                </div>
+            </section>
+            <section class="exo31">
+            <h2>Exercice 31</h2>
+                <div class="resumeWrapper">
+                    <h3>Write a PHP program to swap two variables.</h3>
+                </div>
+                <div class="answerWrapper">
+                    <div class="mySol">
+                        <h3>Let's GO !!</h3>
+                        <?php
+                           
+                        ?>
+                    </div>
+                    <div class="myCode">
+                        <h3>Show my script :</h3>
+                            <p>
+                                &lt?php <br>
+                                   
+                                ?&gt
+                            </p>
+                    </div>
+                    <div class="comment_area">
+                        <p>There's a link to the solution given by the website : <a href="https://www.w3resource.com/php-exercises/php-basic-exercise-19.php">solution</a></p>
+                        <p></p>
+                    </div>
+                </div>
+            </section>
+            <section class="exo32">
+            <h2>Exercice 32</h2>
+                <div class="resumeWrapper">
+                    <h3>Write a PHP program to check whether a number is an Armstrong number or not. Return true if the number is Armstrong otherwise return false..</h3>
+                    <div class="resumeOutputExple">
+                        <p><strong>Note : An Armstrong number of three digits is an integer so that the sum of the cubes of its digits is equal to the number itself. <br> For example, 153 is an Armstrong number since 1**3 + 5**3 + 3**3 = 153</strong> .</p>
+                    </div>
+                </div>
+                <div class="answerWrapper">
+                    <div class="mySol">
+                        <h3>Ready , Go :</h3>
+                        <?php
+                            
+                        ?>
+                    </div>
+                    <div class="myCode">
+                        <h3>Showing my script :</h3>
+                        <p>
+                            &lt?php <br>
+                                
+                            ?&gt <br>
+                        </p>
+                    </div>
+                    <div class="comment_area">
+                        <h3>my point of view</h3>
+                        <p>There's a link to the solution given by the website : <a href="https://www.w3resource.com/php-exercises/php-basic-exercise-20.php">solution</a></p>
+                    </div>
+                </div>
+            </section>
+            <section class="exo33">
+            <h2>Exercice 33</h2>
+                <div class="resumeWrapper">
+                    <h3>Write a PHP program to convert word to digit.</h3>
+                    <div class="resumeOutputExple">
+                        <p><strong>Input: </strong>zero;three;five;six;eight;one</p>
+                        <p><strong>Output: </strong>035681</p>
+                    </div>
+                </div>
+                <div class="answerWrapper">
+                    <div class="mySol">
+                        <h3>Lest's Go </h3>
+                    <?php
+                       
+                    ?>
+                    </div>
+                    <div class="myCode">
+                        <h3>My Script : </h3>
+                        <p>
+                            &lt?php <br>
+                            
+                            ?&gt <br>
+                        </p>
+                    </div>
+                    <div class="comment_area">
+                        <h3>my point of view</h3>
+                        <p>Didn't manage to solve it, i need to do it again and again</p>
+                        <p>share the link to the <a href="https://www.w3resource.com/php-exercises/php-basic-exercise-21.php">solution</a></p>
+                    </div>
+                </div>
+            </section>
+            <section class="exo34">
+            <h2>Exercice 34</h2>
+                <div class="resumeWrapper">
+                    <h3>Write a PHP program to check the bits of the two given positions of a number are same or not. </h3>
+                    <div class="resumeOutputExple">
+                        <p>112 - > 01110000 <br> Test 2nd and 3rd position <br> Result: True <br> Test 4th and 5th position <br> Result: False</p>
+                    </div>
+                </div>
+                <div class="answerWrapper">
+                    <div class="mySol">
+                        <h3>Solved IT !</h3>
+                        <?php
+                            
+                        ?>
+                    </div>
+                    <div class="myCode">
+                        <h3>Show my code</h3>
+                        <p>
+                            &lt?php <br>
+                                
+                            ?&gt 
+                        </p>
+                    </div>
+                    <div class="comment_area">
+                        <h3>my point of view</h3>
+                        <p>There's a link to the solution given by the website : <a href="https://www.w3resource.com/php-exercises/php-basic-exercise-22.php">solution</a></p>
+                        <p></p>
+                    </div>
+                </div>
+            </section>
+            <section class="exo35">
+            <h2>Exercice 35</h2>
+                <div class="resumeWrapper">
+                    <h3>Write a PHP program to remove duplicates from a sorted list . </h3>
+                    <div class="resumeOutputExple">
+                        <p><strong>Input :</strong> (1,1,2,2,3,4,5,5).</p>
+                        <p><strong>Output :</strong> (1,1,2,2,3,4,5,5).</p>
+                    </div>
+                </div>
+                <div class="answerWrapper">
+                    <div class="mySol">
+                        <h3>Go Ahead</h3>
+                        <?php
+                            
+                        ?>
+                    </div>
+                    <div class="myCode">
+                        <h3>Showing my code</h3>
+                        <p>
+                            &lt?php <br>
+                            
+                            ?&gt 
+                        </p>
+                    </div>
+                    <div class="comment_area">
+                        <h3>my point of view</h3>
+                        <p>There's a link to the solution given by the website : <a href="https://www.w3resource.com/php-exercises/php-basic-exercise-23.php">solution</a></p>
+                        <p></p>
+                    </div>
+                </div>
+            </section>
+            <section class="exo36">
+            <h2>Exercice 36</h2>
+                <div class="resumeWrapper">
+                    <h3>Write a PHP program to test if a given string occurs at the end of another given string.</h3>
+                </div>
+                <div class="answerWrapper">
+                    <div class="mySol">
+                        <h3>Let's try </h3>
+                        <?php
+                        
+                        ?>
+                    </div>
+                    <div class="myCode">
+                        <h3> I show you my code</h3>
+                        <p>
+                            &lt?php <br>
+                                
+                            ?&gt <br>
+                        </p>
+                    </div>
+                    <div class="comment_area">
+                        <h3>my point of view</h3>
+                        <p>There's a link to the solution given by the website : <a href="https://www.w3resource.com/php-exercises/php-basic-exercise-24.php">solution</a></p>
+                        <p></p>
+                    </div>
+                </div>
+            </section>
+        </main>
+        <footer>
+            <p>© 2023 akkabb, Inc.</p>
+        </footer>
+    </div>
+</body>
+</html>
