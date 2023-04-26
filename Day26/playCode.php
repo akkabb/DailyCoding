9c0d8cfaea43ba67b0aa60e9f02c3474041f4002
akkabb <akkabb@gmx.fr>
2023-04-26 06:09:32 +0200

    Index done | css done | playcode done | Need to solve all them

---
+++ Day26/playCode.php
@@ -0,0 +1,455 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Daily Coding</title>
+</head>
+<body>
+    <div class="container">
+        <h1>PHP basic</h1>
+        <main>
+            <section class="exo37">
+                <h2>Exercice 37</h2>
+                <div class="resumeWrapper">
+                    <h3>Write a PHP program to compute the sum of the prime numbers less than 100.</h3>
+                    <div class="resumeOutputExple">
+                        <p><strong>Note : </strong> There are 25 prime numbers are there in less than 100 </p>
+                        <p>2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 and sum of all these numbers is 1060.</p>
+                    </div>
+                </div>
+                <div class="answerWrapper">
+                    <div class="mySol">
+                        <h3>I try my best : </h3>
+                        <p class="php">
+                            <?php 
+                                
+                            ?>
+                        </p>
+                    </div>
+                    <div class="myCode">
+                        <h3>I show you my script : </h3>
+                        <p>
+                            &lt?php <br>
+                                <br>
+                            ?&gt
+                        </p>
+                    </div>
+                    <div class="comment_area">
+                        <h3>My point of view :</h3>
+                        <p>
+                           
+                        </p>
+                        <p>There's a link to the solution given by the website : <a href="https://www.w3resource.com/php-exercises/php-basic-exercise-25.php">solution</a></p>
+                    </div>
+                </div>
+            </section>
+            <section class="exo38">
+            <h2>Exercice 38</h2>
+                <div class="resumeWrapper">
+                    <h3>Write a PHP program to valid an email address.</h3>
+                    <div class="answerWrapper">
+                        <div class="mySol">
+                            <h3>Lets GO!!</h3>
+                            <p class="php">
+                                <?php
+                                
+                                ?>
+                            </p>
+                        </div>
+                        <div class="myCode">
+                            <h3>My script :</h3>
+                            <p>
+                                &lt?php <br>
+                                    
+                                ?&gt
+                            </p>
+                        </div>
+                        <div class="comment_area">
+                            <h3>My point of view :</h3>
+                            <p> </p>
+                            <p>There's a link to the solution given by the website : <a href="https://www.w3resource.com/php-exercises/php-basic-exercise-26.php">solution</a></p>
+                        </div>
+                    </div>
+                </section>
+                <section class="exo39">
+                    <h2>Exercice 39</h2>
+                    <div class="resumeWrapper">
+                        <h3>Write a PHP program to get the size of a file.</h3>
+                    </div>
+                    <div class="answerWrapper">
+                        <div class="mySol">
+                            <H3>Try to solve it</H3>
+                            <p class="php">
+                                
+                            </p>
+                            <?php
+                               
+                                
+                            ?> 
+                        </div>
+                        <div class="myCode">
+                            <h3>Sample script : </h3>
+                            <p>
+                                &lt?php <br>
+                                    
+                                ?&gt <br>
+                            </p>
+                        </div>
+                        <div class="comment_area">
+                            <h3>My point of view :</h3>
+                            <p>
+                                </a>
+                            </p>
+                            <p>There's a link to the solution given by the website : <a href="https://www.w3resource.com/php-exercises/php-basic-exercise-27.php">solution</a></p>
+                        </div>
+                    </div>
+                </section>
+            <section class="exo40">
+            <h2>Exercice 40</h2>
+                <div class="resumeWrapper">
+                    <h3>Write a PHP program to calculate the mod of two given integers without using any inbuilt modulus operator.</h3>
+                </div>
+                <div class="answerWrapper">
+                    <div class="mySol">
+                        <h3>I will try my best :</h3>
+                       <p class="php">
+                        <?php
+                        
+                        ?>
+                       </p>
+                    </div>
+                    <div class="myCode">
+                        <h3>Show my script :</h3>
+                        <p>
+                            &lt?php <br>
+                            
+                            ?&gt
+                        </p>
+                    </div>
+                    <div class="comment_area">
+                        <h3>my point of view</h3>
+                        <p></p>
+                        <p>The website <a href="https://www.w3resource.com/php-exercises/php-basic-exercise-28.php">solution</a> </p>
+                        <p></p>
+                    </div>
+                </div>
+            </section>
+            <section class="exo41">
+            <h2>Exercice 41</h2>
+                <div class="resumeWrapper">
+                    <h3>Write a PHP program to print out the multiplication table upto 6*6</h3>
+                    <div class="resumeOutputExple">
+                        <p><strong>Output : </strong></p>
+                        <table> 
+                            <tr>
+                                <td>1</td>
+                                <td>2</td>
+                                <td>3</td>
+                                <td>4</td>
+                                <td>5</td>
+                                <td>6</td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>4</td>
+                                <td>6</td>
+                                <td>8</td>
+                                <td>10</td>
+                                <td>12</td>
+                            </tr>
+                            <tr>
+                                <td>3</td>
+                                <td>6</td>
+                                <td>9</td>
+                                <td>12</td>
+                                <td>15</td>
+                                <td>18</td>
+                            </tr>
+                            <tr>
+                                <td>4</td>
+                                <td>8</td>
+                                <td>12</td>
+                                <td>16</td>
+                                <td>20</td>
+                                <td>24</td>
+                            </tr>
+                            <tr>
+                                <td>5</td>
+                                <td>10</td>
+                                <td>15</td>
+                                <td>20</td>
+                                <td>25</td>
+                                <td>30</td>
+                            </tr>
+                            <tr>
+                                <td>6</td>
+                                <td>12</td>
+                                <td>18</td>
+                                <td>24</td>
+                                <td>30</td>
+                                <td>36</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+                <div class="answerWrapper">
+                    <div class="mySol">
+                        <h3>Go GO GO !!!</h3>
+                        <?php
+                            
+                        ?>
+                    </div>
+                    <div class="myCode">
+                        <h3>Show my script :</h3>
+                        <p>
+                            &lt?php <br>
+                            
+                            ?&gt <br>
+                        </p>
+                    </div>
+                    <div class="comment_area">
+                        <h3>my point of view</h3>
+                        <p></p>
+                        <p>There's a link to the solution given by the website : <a href="https://www.w3resource.com/php-exercises/php-basic-exercise-29.php">solution</a></p>
+                    </div>
+                </div>
+            </section>
+            <section class="exo42">
+            <h2>Exercice 42</h2>
+                <div class="resumeWrapper">
+                    <h3>Write a PHP program to find the first non-repeated character in a given string.</h3>
+                    <div class="resumeOutputExple">
+                        <p><strong>Input :</strong> Green</p>
+                        <p><strong>Output :</strong> G</p>
+                        <p><strong>Input :</strong> abcdea</p>
+                        <p><strong>Output :</strong> b</p>
+                    </div>
+                </div>
+                <div class="answerWrapper">
+                    <div class="mySol">
+                        <h3>Try to solve it :</h3>
+                        <p class="php">
+                            <?php
+                               
+                            ?>
+
+                        </p>
+                    </div>
+                    <div class="myCode">
+                        <h3>Show my script :</h3>
+                            <p>
+                                &lt?php <br>
+                                
+                                ?&gt
+                            </p>
+                    </div>
+                    <div class="comment_area">
+                        <h3>my point of view</h3>
+                        <p></p>
+                        <p>There's a link to the solution given by the website : <a href="https://www.w3resource.com/php-exercises/php-basic-exercise-30.php">solution</a></p>
+                    
+                    </div>
+                </div>
+            </section>
+            <section class="exo43">
+            <h2>Exercice 43</h2>
+                <div class="resumeWrapper">
+                    <h3> Write a PHP program that multiplies corresponding elements of two given lists.</h3>
+                </div>
+                <div class="answerWrapper">
+                    <div class="mySol">
+                        <h3>Let's GO !!</h3>
+                        <p class="php">
+                        <?php
+                        
+                        displayAfterSwap(5, 2);
+                        ?>
+                        </p>
+                    </div>
+                    <div class="myCode">
+                        <h3>Show my script :</h3>
+                            <p>
+                                &lt?php <br>
+                                
+                                ?&gt
+                            </p>
+                    </div>
+                    <div class="comment_area">
+                        <h3>my point of view</h3>
+                        <p>There's a link to the solution given by the website : <a href="https://www.w3resource.com/php-exercises/php-basic-exercise-19.php">solution</a></p>
+                        <p></p>
+                    </div>
+                </div>
+            </section>
+            <section class="exo44">
+            <h2>Exercice 44</h2>
+                <div class="resumeWrapper">
+                    <h3>Write a PHP program to print out the sum of pairs of numbers of a given sorted array of positive integers which is equal to a given number</h3>
+                    
+                </div>
+                <div class="answerWrapper">
+                    <div class="mySol">
+                        <h3>Ready , Go :</h3>
+                        <?php
+                            
+                        ?>
+                    </div>
+                    <div class="myCode">
+                        <h3>Showing my script :</h3>
+                        <p>
+                            &lt?php <br>
+                                
+                            ?&gt <br>
+                        </p>
+                    </div>
+                    <div class="comment_area">
+                        <h3>my point of view</h3>
+                        <p></p>
+                        <p>There's a link to the solution given by the website : <a href="">solution</a></p>
+                    </div>
+                </div>
+            </section>
+            <section class="exo45">
+            <h2>Exercice 45</h2>
+                <div class="resumeWrapper">
+                    <h3>Write a PHP program to compute the sum of the digits of a number.</h3>
+                    
+                </div>
+                <div class="answerWrapper">
+                    <div class="mySol">
+                        <h3>Lest's Go </h3>
+                    <?php
+                       
+                    ?>
+                    </div>
+                    <div class="myCode">
+                        <h3>My Script : </h3>
+                        <p>
+                            &lt?php <br>
+                            
+                            ?&gt <br>
+                        </p>
+                    </div>
+                    <div class="comment_area">
+                        <h3>my point of view</h3>
+                        <p></p>
+                        <p>
+                           
+                        </p>
+                        <p>share the link to the <a href="https://www.w3resource.com/php-exercises/php-basic-exercise-21.php">solution</a></p>
+                    </div>
+                </div>
+            </section>
+            <section class="exo46">
+            <h2>Exercice 46</h2>
+                <div class="resumeWrapper">
+                    <h3>Write a PHP program to find heights of the top three building in descending order from eight given buildings. </h3>
+                    <div class="resumeOutputExple">
+                        <p><strong>Input :</strong> 0 = height of building (integer) = 10,000 </p>
+                        <p><strong>Output :</strong></p>
+                        <p> Heights of the top three buildings:  <br> 45 <br> 25 <br> 24 </p>
+                    </div>
+                </div>
+                <div class="answerWrapper">
+                    <div class="mySol">
+                        <h3>Solved IT !</h3>
+                        <p class="php">
+                        <?php
+                            
+                        ?>
+                        </p>
+                    </div>
+                    <div class="myCode">
+                        <h3>Show my code</h3>
+                        <p>
+                            &lt?php <br>
+                            
+                            ?&gt 
+                        </p>
+                    </div>
+                    <div class="comment_area">
+                        <h3>my point of view</h3>
+                        <p></p>
+                        <p>There's a link to the solution given by the website : <a href="https://www.w3resource.com/php-exercises/php-basic-exercise-34.php">solution</a></p>
+                        <p></p>
+                    </div>
+                </div>
+            </section>
+            <section class="exo47">
+            <h2>Exercice 47</h2>
+                <div class="resumeWrapper">
+                    <h3>Write a PHP program to compute the digit number of sum of two given integers</h3>
+                    <div class="resumeOutputExple">
+                        <p><strong>Input :</strong> Each test case consists of two non-negative integers x and y which are separated by a space in a line. <br>
+0 = x, y = 1,000,000</p>
+                        <p><strong>Output :</strong>  Digit number of sum of two given integers: 2</p>
+                    </div>
+                </div>
+                <div class="answerWrapper">
+                    <div class="mySol">
+                        <h3>Go Ahead</h3>
+                        <p class="php">
+                        <?php
+                            
+                        ?>
+                        </p>
+                    </div>
+                    <div class="myCode">
+                        <h3>Showing my code</h3>
+                        <p>
+                            &lt?php <br>
+                            
+                            ?&gt 
+                        </p>
+                    </div>
+                    <div class="comment_area">
+                        <h3>my point of view</h3>
+                        <p>There's a link to the solution given by the website : <a href="https://www.w3resource.com/php-exercises/php-basic-exercise-35.php">solution</a></p>
+                        <p> </p>
+                    </div>
+                </div>
+            </section>
+            <section class="exo48">
+            <h2>Exercice 48</h2>
+                <div class="resumeWrapper">
+                    <h3>Write a PHP program to check whether three given lengths (integers) of three sides form a right triangle. <br> Print "Yes" if the given sides form a right triangle otherwise print "No"</h3>
+                        <div class="resumeOutputExple">
+                            <p><strong>Input :</strong> </p>
+                            <p>Integers separated by a single space. <br> 1 = length of the side = 1,000</p>
+                            <p><strong>Output :</strong> </p>
+                            <p>Yes</p>
+                        </div>
+                </div>
+                <div class="answerWrapper">
+                    <div class="mySol">
+                        <h3>Let's try </h3>
+                        <?php
+                        
+                        ?>
+                    </div>
+                    <div class="myCode">
+                        <h3> I show you my code</h3>
+                        <p>
+                            &lt?php <br>
+                           
+                            ?&gt <br>
+                        </p>
+                    </div>
+                    <div class="comment_area">
+                        <h3>my point of view</h3>
+                        <p></p>
+                        <p>There's a link to the solution given by the website : <a href="https://www.w3resource.com/php-exercises/php-basic-exercise-36.php">solution</a></p>
+                        <p></p>
+                    </div>
+                </div>
+            </section>
+        </main>
+        <footer>
+            <p>© 2023 akkabb, Inc.</p>
+        </footer>
+    </div>
+</body>
+</html>
